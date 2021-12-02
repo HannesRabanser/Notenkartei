1473538cf8787ac3b9ace8128abe7158cf057b04
HannesRabanser <92477045+HannesRabanser@users.noreply.github.com>
2021-12-02 08:01:01 +0100

    login erstellt

---
+++ Website/searchSite.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<?php
+session_start();
+?>
+<html> 
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style/main.css">
+        <link rel="stylesheet" href="style/login.css">
+    </head> 
+    <body>
+        <?php
+        print_r($_SESSION);
+        ?>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
